33064c16e9fcc68b15625c4d5421e4b08a32d086
Daegonny <igorchagasm@hotmail.com>
2021-09-03 08:01:59 -0300

    torna variáveis locais melhorando escopo

--- hamming/hamming.lua
+++ hamming/hamming.lua
@@ -1,13 +1,13 @@
 local Hamming = {}
 
 function Hamming.compute(a,b)
-	len_a = a:len()
-	len_b = b:len()
+	local len_a = a:len()
+	local len_b = b:len()
 	if len_a ~= len_b then
 		return -1
 	end
 
-	distance = 0
+	local distance = 0
 	for i=1,len_a do
 		if a:sub(i, i) ~= b:sub(i, i) then
 			distance = distance + 1
